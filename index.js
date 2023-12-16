d1874cca9a5ad55cf838e71446658421280e5024
rafaelmasselli <rafaelmasselli0@gmail.com>
2023-12-16 18:21:50 -0300

    feat: create performance test

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const router = require("./src/router/index.router");
 const express = require("express");
+const v8 = require("v8");
+
+const router = require("./src/router/index.router");
+
 const app = express();
 const port = 3000;
 
+v8.setFlagsFromString("--max_old_space_size=8192");
 app.use(express.json());
 app.use("/", router);
 
--- src/services/memoryIntensive.service.js
+++ src/services/memoryIntensive.service.js
@@ -2,6 +2,7 @@ class memoryIntensiveService {
   handle() {
     const largeArray = new Array(1e6).fill("some data");
     const processedArray = largeArray.map((item) => item.toUpperCase());
+    largeArray.length = 0;
     return processedArray;
   }
 }
