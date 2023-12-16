31569e3e1245b2815d1e672f3ebdf81ce34a794e
rafaelmasselli <rafaelmasselli0@gmail.com>
2023-12-16 18:22:03 -0300

    feat: create performance test

--- src/middleware/monitorMemory.middleware.js
+++ src/middleware/monitorMemory.middleware.js
@@ -1,7 +1,12 @@
+const os = require("os-utils");
+
 class monitorMemoryMiddleware {
   handle(next) {
     const memoryUsage = process.memoryUsage();
     console.log(`Memory Usage: ${JSON.stringify(memoryUsage)}`);
+    os.cpuUsage(function (v) {
+      console.log("CPU Usage (%): " + v);
+    });
     next();
   }
 }
